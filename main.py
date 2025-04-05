801dac2840d15dcdb395be23d416ca9dc75e71fa
Nikhil-Penn <nruia@seas.upenn.edu>
2025-04-04 23:50:58 -0400

    current without timeseries

--- main.py
+++ main.py
@@ -59,57 +59,6 @@ from flask import request, jsonify
 from datetime import datetime
 from models import db, TrafficEntry
 
-# @app.route('/filter', methods=['GET'])
-# def get_filtered_data():
-#     # Get query params
-#     datetime_start_str = request.args.get('datetime_start')
-#     datetime_end_str = request.args.get('datetime_end')
-#     detection_group = request.args.get('detection_group')
-#     vehicle_class = request.args.get('vehicle_class')
-
-#     # Validate required fields
-#     if not datetime_start_str or not datetime_end_str:
-#         return jsonify({'error': 'datetime_start and datetime_end are required.'}), 400
-
-#     try:
-#         # Convert to datetime objects for comparison
-#         datetime_start = datetime.strptime(datetime_start_str, "%Y-%m-%d %H:%M:%S")
-#         datetime_end = datetime.strptime(datetime_end_str, "%Y-%m-%d %H:%M:%S")
-#     except ValueError:
-#         return jsonify({'error': 'Datetime must be in format YYYY-MM-DD HH:MM:SS'}), 400
-
-#     # Start base query
-#     query = TrafficEntry.query.filter(
-#         TrafficEntry.datetime >= datetime_start_str,
-#         TrafficEntry.datetime <= datetime_end_str
-#     )
-
-#     # Optional filters
-#     if detection_group:
-#         query = query.filter(TrafficEntry.detection_group == detection_group)
-
-#     if vehicle_class:
-#         query = query.filter(TrafficEntry.vehicle_class == vehicle_class)
-
-#     # Execute and return
-#     results = query.all()
-#     ## total number per vehicle class
-#     ## total number of vehicles
-#     ## total price per vehicle class
-#     ## total price
-
-
-#     return jsonify([{
-#         'id': e.id,
-#         'datetime': e.datetime,
-#         'is_peak': e.is_peak,
-#         'vehicle_class': e.vehicle_class,
-#         'detection_group': e.detection_group,
-#         'crz_entries': e.crz_entries,
-#         'excluded_roadway_entries': e.excluded_roadway_entries
-#     } for e in results])
-
-
 @app.route('/filter', methods=['GET'])
 def get_filtered_data():
     datetime_start = request.args.get('datetime_start')
